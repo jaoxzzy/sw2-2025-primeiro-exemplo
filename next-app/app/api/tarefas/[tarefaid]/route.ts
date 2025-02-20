3dcc9894cf86451f38976531d251f5ae01fb7221
João Pedro <joao.galvao25@etec.sp.gov.br>
2025-02-20 01:50:35 +0000

    Aula parte 2 19/02/2025

---
+++ next-app/app/api/tarefas/[tarefaid]/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+
+import tasks from '@/dados.json';
+
+export async function GET(request: Request, context: any) {
+    const { params } = context;
+    let resultado = null;
+
+    tasks.forEach((item) => {
+        if(item.id == params. tarefaid) {
+            resultado = item;
+        }
+    });
+
+    return NextResponse.json(resultado)
+}
